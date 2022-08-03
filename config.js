fd205727b84b448f7e215c98c973b1a7e7e43b19
RevnSukaTuru <109857886+RevnSukaTuru@users.noreply.github.com>
2022-08-03 19:21:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6285760165634','6282235537809']
+global.premium = ['6285760165634']
+global.packname = 'VBOTZ'
+global.author = 'Vnzz'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
